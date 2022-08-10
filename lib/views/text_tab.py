323e297bf82db0b81ef1133a66daa74a0f6d6184
Borie, Niels <niels.borie@bleckwen.ai>
2022-08-10 20:37:52 +0200

    :art: Add text & datetime views

---
+++ lib/views/text_tab.py
@@ -0,0 +1,30 @@
+from typing import List
+
+import pandas as pd
+import streamlit as st
+
+from lib.consistency.consistency import reference_consistency
+from lib.core.schema import Mappings
+
+
+def display_text_consistency(text_col: str, df1: pd.DataFrame, df2: pd.DataFrame):
+    st.subheader("Text consistency")
+    same_id, intersection = reference_consistency(df1, df2, text_col)
+    st.caption("Same text")
+    st.write(same_id)
+    st.caption("text intersection %")
+    st.write(100 * intersection / len(df1))
+
+
+def text_consistency_view(mapping_schema: Mappings, df1: pd.DataFrame, df2: pd.DataFrame):
+    st.markdown("""---""")
+    if mapping_schema is None:
+        mapping_schema = Mappings({})
+    if df1 is not None:
+        if df2 is not None:
+            text_fields: List[str] = [field.name for field in mapping_schema.get_text_fields()]
+            text_1 = [col for col in df1.columns if col in text_fields]
+            text_2 = [col for col in df2.columns if col in text_fields]
+            text_cols = list(set(text_1).intersection(set(text_2)))
+            for text_col in text_cols:
+                display_text_consistency(text_col, df1, df2)
