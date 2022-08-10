9f692f831c1a06875e6adc501c44a93c94d31ec0
Borie, Niels <niels.borie@bleckwen.ai>
2022-08-10 20:37:52 +0200

    :art: Add text & datetime views

---
+++ lib/views/datetime_tab.py
@@ -0,0 +1,43 @@
+from typing import Dict, List
+import streamlit as st
+import pandas as pd
+
+from lib.annotation.annotation import annotated_text, annotation
+from lib.consistency.consistency import plot_gantt
+from lib.core.schema import Mappings
+
+
+def display_datetime_consistency(datetime_col: str, a: pd.Series, b: pd.Series):
+    annotated_text(
+        (datetime_col, "", "#BBA5FF"),
+        annotation("", "")
+    )
+    fig, periods = plot_gantt(a, b, datetime_col)
+    col1_period, col2_period, col3_period = st.columns([1, 1, 1])
+    col1_period.caption("")
+    col2_period.caption("min")
+    col3_period.caption("max")
+    col1_period.caption("df1")
+    col2_period.write(periods[0][0])
+    col3_period.write(periods[0][1])
+    col1_period.caption("df2")
+    col2_period.write(periods[1][0])
+    col3_period.write(periods[1][1])
+    st.plotly_chart(fig, use_container_width=True)
+    st.markdown("""---""")
+
+
+def datetime_consistency_view(mapping_schema: Mappings, df1: pd.DataFrame, df2: pd.DataFrame):
+    st.markdown("""---""")
+    if mapping_schema is None:
+        mapping_schema = Mappings({})
+    if df1 is not None:
+        if df2 is not None:
+            datetime_fields: List[str] = [field.name for field in mapping_schema.get_datetime_fields()]
+            datetime_1 = [col for col in df1.columns if col in datetime_fields]
+            datetime_2 = [col for col in df2.columns if col in datetime_fields]
+            datetime_cols = list(set(datetime_1).intersection(set(datetime_2)))
+            for datetime_col in datetime_cols:
+                a = pd.to_datetime(df1[datetime_col])
+                b = pd.to_datetime(df2[datetime_col])
+                display_datetime_consistency(datetime_col, a, b)
